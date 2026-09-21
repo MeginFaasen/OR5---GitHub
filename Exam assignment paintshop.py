20ab201d7396f277bdc55ae36514773d5eb4923d
amvanderheij <431952@student.fontys.nl>
2026-09-21 14:14:10 +0200

    Update Exam assignment paintshop.py

--- Exam assignment paintshop.py
+++ Exam assignment paintshop.py
@@ -9,5 +9,28 @@ print(dfs.head(12))
 
 dfo = dfo.sort_values(by=["Deadline"], ascending=True)
 print(dfo.head(10), '\n')
+e1 = 0
+e2 = 0
+e3 = 0
+ma1 = []
+ma2 = []
+ma3 = []
 
-#for i in dfo.index:hooiiii
+for i in dfo.index:
+    ec1 = e1 + (dfo.loc[i, "Surface"]/dfm.loc[0, "Speed"]) #+ setuptijd
+    ec2 = e2 + (dfo.loc[i, "Surface"]/dfm.loc[1, "Speed"]) #+ setuptijd
+    ec3 = e3 + (dfo.loc[i, "Surface"]/dfm.loc[2, "Speed"]) #+ setuptijd
+    if ec1 == min(ec1, ec2, ec3):
+        ma1.append(dfo.loc[i, "Order"])
+        e1 += ec1
+    elif ec2 ==min(ec1, ec2, ec3):
+        ma2.append(dfo.loc[i, "Order"])
+        e2 += ec2
+    else:
+        ma3.append(dfo.loc[i, "Order"])
+        e3 += ec3
+print(f'Machine 1: {ma1} \n')
+print(f'Machine 2: {ma2} \n')
+print(f'Machine 3: {ma3} \n')
+
+# eindtijd = eindtijd + surface/machinatijd + setuptijd
