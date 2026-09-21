2947fba0734c884fc4957419b014e2b7db94fbf2
amvanderheij <431952@student.fontys.nl>
2026-09-21 15:10:32 +0200

    Update Exam assignment paintshop.py

--- Exam assignment paintshop.py
+++ Exam assignment paintshop.py
@@ -7,19 +7,37 @@ dfs = pd.read_excel('PaintShop - September 2026.xlsx', 'Setups')
 print(dfm.head())
 print(dfs.head(12))
 
-dfo = dfo.sort_values(by=["Deadline"], ascending=True)
+dfo = dfo.sort_values(by=["Deadline"], ascending=True) #greedy rule first deadline first
 print(dfo.head(10), '\n')
+
+#eindtijden 
 e1 = 0
 e2 = 0
 e3 = 0
+
+#machine orders
 ma1 = []
 ma2 = []
 ma3 = []
 
+# su1 setuptijd machine 1
+# su2 setuptijd machine 2
+# su3 setuptijd machine 3
+# colour = dfo.loc[dfo["Order"] == "ORD11", "Colour"].item() 
+# print(colour)
+
+
 for i in dfo.index:
-    ec1 = e1 + (dfo.loc[i, "Surface"]/dfm.loc[0, "Speed"]) #+ setuptijd
-    ec2 = e2 + (dfo.loc[i, "Surface"]/dfm.loc[1, "Speed"]) #+ setuptijd
-    ec3 = e3 + (dfo.loc[i, "Surface"]/dfm.loc[2, "Speed"]) #+ setuptijd
+    newcol = dfo.loc[i, "Colour"]
+    # col1 = dfo.loc[dfo["Order"] == ma1[-1], "Colour"].item()
+    # col2 = dfo.loc[dfo["Order"] == ma2[-1], "Colour"].item()
+    # col3 = dfo.loc[dfo["Order"] == ma3[-1], "Colour"].item()
+    print(newcol)
+
+
+    ec1 = e1 + (dfo.loc[i, "Surface"]/dfm.loc[0, "Speed"]) #+ su1
+    ec2 = e2 + (dfo.loc[i, "Surface"]/dfm.loc[1, "Speed"]) #+ su2
+    ec3 = e3 + (dfo.loc[i, "Surface"]/dfm.loc[2, "Speed"]) #+ su3
     if ec1 == min(ec1, ec2, ec3):
         ma1.append(dfo.loc[i, "Order"])
         e1 += ec1
